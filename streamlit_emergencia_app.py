c9c95ee6ef179056cec130c60e7fd0556bce6f12
GCHANTRE <78544473+GUILLE-bit@users.noreply.github.com>
2025-08-08 13:49:28 -0300

    Update streamlit_emergencia_app.py

--- streamlit_emergencia_app.py
+++ streamlit_emergencia_app.py
@@ -90,7 +90,7 @@ umbral_usuario = st.sidebar.number_input(
     "Umbral de EMEAC para 100%",
     min_value=0.5,
     max_value=2.84,
-    value=1.325,
+    value=1.75,
     step=0.01,
     format="%.2f"
 )
